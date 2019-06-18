dc994272525266e999b3da477c3d9ffd43becd7c
Antberry <aniej2013@gmail.com>
2019-06-18 09:46:13 -0700

    initial commit, new spring app

---
+++ src/main/java/com/berry/Anthony/codefellowshipp/CodefellowshippApplication.java
@@ -0,0 +1,13 @@
+package com.berry.Anthony.codefellowshipp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodefellowshippApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodefellowshippApplication.class, args);
+	}
+
+}
